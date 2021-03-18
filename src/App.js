ce355441c482747c01805d3013fe6c7c638e534e
Darya Slugina <d.slugina@gmail.com>
2021-03-18 23:45:23 +0200

    Created router

--- src/App.js
+++ src/App.js
@@ -1,54 +1,38 @@
 import "./App.css";
-import TinderDate from "./TinderDate/TinderDate";
 import { BrowserRouter, Route, Link, Switch, Redirect } from "react-router-dom";
-import Profile from "./ProfilePage/Profile";
-import Login from "./Login/Login";
-import Matches from "./Matches/Matches";
+import HomePage from "./HomePage/HomePage";
+import HomePageLoggedIn from "./HomePage/HomePageLoggedIn";
+import Registration from "./RegistrationRage/Registration";
+import Categories from "./CategoriesPage/Categories";
+import Genres from "./GenresPage/Genres";
+import Footer from "./Footer/Footer";
 import React from "react";
 
 function App() {
-  const element = <h1>Pesho</h1>;
-  const username = "Slatkata93";
   const isLoggedIn = true;
-  const isAdmin = false;
 
   return (
     <BrowserRouter>
       <div className="App">
-        <h2>Zdraveite tova e moq sait za zapoznastva. </h2>
-
-        <button className="button">Test Button</button>
-
-        <nav className="header">
-          <Link to="/">Home</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/matches">Matches</Link>cd
-          <Link to="/login">Login</Link>
-        </nav>
-
         <Switch>
           <Route exact path="/">
-            {isLoggedIn ? <TinderDate /> : <Redirect to="/login" />}
+          {isLoggedIn ? <HomePageLoggedIn /> : <HomePage/>}
           </Route>
 
-          {/* {isLoggedIn && (
-            <React.Fragment> */}
-          <Route path="/profile">
-            <Profile />
+          <Route path="/registration">
+            <Registration />
           </Route>
 
-          <Route path="/adminPanel">
-            {isAdmin ? <h3>This is the admin panel</h3> : <Redirect to="/" />}
+          <Route path="/categories">
+            <Categories />
           </Route>
 
-          <Route path="/matches/:matchId">
-            <Matches />
+          <Route path="/genres">
+            <Genres />
           </Route>
-          {/* </React.Fragment>
-          )} */}
 
-          <Route path="/login">
-            <Login />
+          <Route path="/books/:bookId">
+            <Books />
           </Route>
 
           <Route path="*">
@@ -56,7 +40,7 @@ function App() {
           </Route>
         </Switch>
 
-        <footer>This is my footer</footer>
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import styles from './Footer.module.css';
+
+export default function Footer() {
+    return (
+        <div className={styles.siteFooter}>
+            <div className={styles.footerContents}>
+                <div className={styles.contentRow}>
+
+                </div>
+            </div>
+        </div>
+    )
+}

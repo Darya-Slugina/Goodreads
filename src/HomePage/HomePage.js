bc59d716f3d807d4185a7cf6567f381fcaa43577
OlgaKaikchiyan <olga.kaikchiyan@gmail.com>
2021-03-19 21:03:41 +0200

    editing styles

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import styles from "./HomePage.module.css"
+import "../styles/HomePage.css"
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import React from "react"

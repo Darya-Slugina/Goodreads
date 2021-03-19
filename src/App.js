b639062a857e1120e9ab2fe05519b546827f3bda
OlgaKaikchiyan <olga.kaikchiyan@gmail.com>
2021-03-19 20:39:48 +0200

    started working on the HomePage layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./HomePage/HomePage";
 import HomePageLoggedIn from "./HomePage/HomePageLoggedIn";

3f18914dc7360e9bd63c92d534a5f199f857c3e0
OlgaKaikchiyan <olga.kaikchiyan@gmail.com>
2021-03-22 16:46:25 +0200

    finalizing header (logged in)

--- src/HomePage/HomePageLoggedIn.js
+++ src/HomePage/HomePageLoggedIn.js
@@ -37,39 +37,9 @@ export default function HomePageLoggedIn() {
                                 <Dropdown.Item href="#/action-2" className="sign-out-link">Sign out</Dropdown.Item>
                             </Dropdown.Menu>
                         </Dropdown>
-
-                        {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
-                        {/* <a href="/"></a>
-                        <div className="dropdown-profile">
-                            <div className="dropdown-profile-wrapper">
-                                <span className="profile-name">Olga</span>
-                                <ul>
-                                    <li><a href="" className="sign-out-link">Sign out</a></li>
-                                </ul>
-                            </div>
-                        </div> */}
                     </nav>
                 </div>
             </header>
-            <main>
-                <div className="main-container">
-                    <div className="text-box-wrapper">
-                        <div className="text-box" style={{ marginRight: "40px" }}>
-                            <h2>Deciding what to read next?</h2>
-                            <p>You’re in the right place. Tell us what titles or genres you’ve enjoyed in the past, and we’ll give you surprisingly insightful recommendations.</p>
-                        </div>
-                        <div className="text-box">
-                            <h2>What are your friends reading?</h2>
-                            <p>Chances are your friends are discussing their favorite (and least favorite) books on Goodreads.</p>
-                        </div>
-                    </div>
-                    <div className="discovery-box"></div>
-                    <div id="browseBox">
-                        <h2>Search and browse books</h2>
-                        <input type="text" placeholder="Title / Author" />
-                    </div>
-                </div>
-            </main>
         </React.Fragment>
     )
 
